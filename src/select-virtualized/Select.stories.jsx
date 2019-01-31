6327ed98d5fc0b7d55a044a0cdadf8c199ccd130
Guillermo Polit <guiyep@gmail.com>
2019-01-31 16:08:32 +1100

    updating stories

--- src/select-virtualized/Select.stories.jsx
+++ src/select-virtualized/Select.stories.jsx
@@ -15,15 +15,13 @@ const optionsDefault = new Array(20).fill(null).map(() => ({
 const defaultValue = { ...optionsDefault[random.int(1, 19)] };
 
 const buildOptionsSize = (size) => {
-  let count = 0;
   return new Array(size / optionsDefault.length).fill(null).reduce(
     (acc) =>
       acc.concat(
         optionsDefault.map(({ value, label, extra }) => {
-          count += 1;
           return {
             value: `${value}-${random.guid()}`,
-            label: `${label} ${count || ''}`,
+            label: `${label} speaks ${random.language()}`,
             extra,
           };
         }),
@@ -56,164 +54,108 @@ storiesOf(`React Select Virtualized`, module)
       maxPropsIntoLine: 1,
     },
   })
-  .add('Basic', () => <Select options={optionsDefault} />, {
-    notes: 'This is the basic definition for a select',
+  .add('Basic', () => <Select options={optionsDefault} />)
+  .add('with default value', () => <Select defaultValue={defaultValue} options={optionsDefault} />)
+  .add('with 1500 elements', () => <Select options={op1500} />)
+  .add('with 2500 elements', () => <Select options={ops2500} />)
+  .add('with 4500 elements', () => <Select options={ops4500} />)
+  .add('with 6000 elements', () => <Select options={ops6000} />)
+  .add('with 8000 elements', () => <Select options={ops8000} />)
+  .add('with 10500 elements', () => <Select options={ops10500} />)
+  .add('disabled select', () => <Select options={optionsDefault} isDisabled />)
+  .add('clear select input', () => {
+    const selectRef = React.createRef();
+    return (
+      <Fragment>
+        <button style={{ width: '50px' }}>
+          {' '}
+          onClick={() => selectRef.current.clear()}>Click me for clearing the Select default value
+        </button>
+        <br />
+        <Select ref={selectRef} defaultValue={defaultValue} options={optionsDefault} />
+      </Fragment>
+    );
   })
-  .add('with default value', () => <Select defaultValue={defaultValue} options={optionsDefault} />, {
+  .add('focus select input', () => {
+    const selectRef = React.createRef();
+    return (
+      <Fragment>
+        <button style={{ width: '50px' }}>
+          {' '}
+          onClick={() => selectRef.current.focus()}>Click me for focusing the Select
+        </button>
+        <br />
+        <Select ref={selectRef} defaultValue={defaultValue} options={optionsDefault} />
+      </Fragment>
+    );
+  })
+  .add('callbacks action wit logs', () => (
+    <Select
+      options={optionsDefault}
+      onBlur={action('onBlur happened')}
+      onValueChange={action('onValueChange happened')}
+    />
+  ))
+  .add('empty options in the select', () => <Select noOptionsMessage={() => 'No Items...'} options={[]} />, {
     notes: '',
   })
-  .add(
-    'with 1500 elements',
-    () => {
-      return <Select options={op1500} />;
-    },
-    {
-      notes: '',
-    },
-  )
-  .add('with 2500 elements', () => <Select options={ops2500} />, {
-    notes: '',
+  .add('select with custom labels format', () => {
+    const labelFormat = ({ label, extra }, { context }) => {
+      if (context === 'value') return `${label} - ${extra}`;
+      return `${label} - ${extra}`;
+    };
+
+    return <Select options={ops2500} defaultValue={defaultValue} formatOptionLabel={labelFormat} />;
   })
-  .add('with 4500 elements', () => <Select options={ops4500} />, {
-    notes: '',
+  .add('grouped default', () => {
+    const ops = [
+      { label: `Group ${random.maleFirstName()}`, options: group1 },
+      { label: `Group ${random.maleFirstName()}`, options: group2 },
+      { label: `Group ${random.maleFirstName()}`, options: group3 },
+      { label: `Group ${random.maleFirstName()}`, options: group4 },
+      { label: `Group ${random.maleFirstName()}`, options: group5 },
+      { label: `Group ${random.maleFirstName()}`, options: group6 },
+      { label: `Group ${random.maleFirstName()}`, options: group7 },
+      { label: `Group ${random.maleFirstName()}`, options: group8 },
+    ];
+
+    return <Select options={ops} defaultValue={defaultValue} grouped />;
   })
-  .add('with 6000 elements', () => <Select options={ops6000} />, {
-    notes: '',
-  })
-  .add('with 8000 elements', () => <Select options={ops8000} />, {
-    notes: '',
-  })
-  .add('with 10500 elements', () => <Select options={ops10500} />, {
-    notes: '',
-  })
-  .add('disabled', () => <Select options={optionsDefault} isDisabled />, {
-    notes: '',
-  })
-  .add(
-    'clear element',
-    () => {
-      const selectRef = React.createRef();
-      return (
-        <Fragment>
-          <button onClick={() => selectRef.current.clear()}>Click me for clearing the Select default value</button>
-          <br />
-          <Select ref={selectRef} defaultValue={defaultValue} options={optionsDefault} />
-        </Fragment>
-      );
-    },
-    {
-      notes: '',
-    },
-  )
-  .add(
-    'focus input',
-    () => {
-      const selectRef = React.createRef();
-      return (
-        <Fragment>
-          <button onClick={() => selectRef.current.focus()}>Click me for focusing the Select</button>
-          <br />
-          <Select ref={selectRef} defaultValue={defaultValue} options={optionsDefault} />
-        </Fragment>
-      );
-    },
-    {
-      notes: '',
-    },
-  )
-  .add(
-    'callbacks',
-    () => (
+  .add('grouped custom format', () => {
+    const ops = [
+      { label: `Group ${random.maleFirstName()}`, options: group1 },
+      { label: `Group ${random.maleFirstName()}`, options: group2 },
+      { label: `Group ${random.maleFirstName()}`, options: group3 },
+      { label: `Group ${random.maleFirstName()}`, options: group4 },
+      { label: `Group ${random.maleFirstName()}`, options: group5 },
+      { label: `Group ${random.maleFirstName()}`, options: group6 },
+      { label: `Group ${random.maleFirstName()}`, options: group7 },
+      { label: `Group ${random.maleFirstName()}`, options: group8 },
+    ];
+
+    // this can be a css also
+    const groupStyle = {
+      background: 'lightcoral',
+      height: '4em',
+      lineHeight: '4em',
+      textAlign: 'center',
+      fontFamily: 'monospace',
+    };
+
+    const groupHeaderHeight = 50;
+
+    const groupFormat = ({ label, options }) => (
+      <div style={groupStyle}>
+        {label} --- ({options.length}) items in this group
+      </div>
+    );
+
+    return (
       <Select
-        options={optionsDefault}
-        onBlur={action('onBlur happened')}
-        onValueChange={action('onValueChange happened')}
+        options={ops}
+        defaultValue={defaultValue}
+        formatGroupHeaderLabel={groupFormat}
+        groupHeaderHeight={groupHeaderHeight}
       />
-    ),
-    {
-      notes: '',
-    },
-  )
-  .add('empty options', () => <Select noOptionsMessage={() => 'No Items To Display'} options={[]} />, {
-    notes: '',
-  })
-  .add(
-    'custom label option',
-    () => {
-      const labelFormat = ({ label, extra }, { context }) => {
-        // this can be any JSX
-        if (context === 'value') return `${label} - ${extra}`;
-        // this can be any JSX
-        return `${label} - ${extra}`;
-      };
-
-      return <Select options={ops2500} defaultValue={defaultValue} formatOptionLabel={labelFormat} />;
-    },
-    {
-      notes: '',
-    },
-  )
-  .add(
-    'grouped default',
-    () => {
-      const ops = [
-        { label: `Group ${random.maleFirstName()}`, options: group1 },
-        { label: `Group ${random.maleFirstName()}`, options: group2 },
-        { label: `Group ${random.maleFirstName()}`, options: group3 },
-        { label: `Group ${random.maleFirstName()}`, options: group4 },
-        { label: `Group ${random.maleFirstName()}`, options: group5 },
-        { label: `Group ${random.maleFirstName()}`, options: group6 },
-        { label: `Group ${random.maleFirstName()}`, options: group7 },
-        { label: `Group ${random.maleFirstName()}`, options: group8 },
-      ];
-
-      return <Select options={ops} defaultValue={defaultValue} grouped />;
-    },
-    {
-      notes: '',
-    },
-  )
-  .add(
-    'grouped custom format',
-    () => {
-      const ops = [
-        { label: `Group ${random.maleFirstName()}`, options: group1 },
-        { label: `Group ${random.maleFirstName()}`, options: group2 },
-        { label: `Group ${random.maleFirstName()}`, options: group3 },
-        { label: `Group ${random.maleFirstName()}`, options: group4 },
-        { label: `Group ${random.maleFirstName()}`, options: group5 },
-        { label: `Group ${random.maleFirstName()}`, options: group6 },
-        { label: `Group ${random.maleFirstName()}`, options: group7 },
-        { label: `Group ${random.maleFirstName()}`, options: group8 },
-      ];
-
-      // this can be a css also
-      const groupStyle = {
-        background: 'lightcoral',
-        height: '4em',
-        lineHeight: '4em',
-        textAlign: 'center',
-        fontFamily: 'monospace',
-      };
-
-      const groupHeaderHeight = 50;
-
-      const groupFormat = ({ label, options }) => (
-        <div style={groupStyle}>
-          {label} --- ({options.length}) items in this group
-        </div>
-      );
-
-      return (
-        <Select
-          options={ops}
-          defaultValue={defaultValue}
-          formatGroupHeaderLabel={groupFormat}
-          groupHeaderHeight={groupHeaderHeight}
-        />
-      );
-    },
-    {
-      notes: '',
-    },
-  );
+    );
+  });
